9f89a97f5d2f438c59ad56ddc13700e642f3e089
artem.khrolenko <artem.khrolenko@gmail.com>
2020-07-12 21:02:58 +0300

    Remake content calculation in T04 and T05

--- TechParamsCalc/Parameters/Content.cs
+++ TechParamsCalc/Parameters/Content.cs
@@ -10,14 +10,14 @@ using TechDotNetLib.Lab.Substances;
 namespace TechParamsCalc.Parameters
 {
     internal class Content : Characteristic
-    {        
+    {
         public short Sel { get; set; }
-        public short ValHmi { get; set; }        
+        public short ValHmi { get; set; }
         public short[] ValCalc { get; set; }
         public short[] DeltaT { get; set; }
         public short[] DeltaP { get; set; }
-        public string[] PercDescription { get; set; }       
-        
+        public string[] PercDescription { get; set; }
+
         public Temperature Temperature { get; set; }
         public Pressure Pressure { get; set; }
         public short AtmoPressure { get; set; }
@@ -26,14 +26,14 @@ namespace TechParamsCalc.Parameters
         public static int id = 0;
 
         public Content(string tagName) : base(tagName)
-        {                     
+        {
             Id = ++id;
         }
 
         //Метод расчета крепости с подключением библиотеки TechDotNetLib
         public double[] CalculateContent()
         {
-            double[] _contentValue = new double[5];            
+            double[] _contentValue = new double[5];
 
             try
             {
@@ -41,17 +41,14 @@ namespace TechParamsCalc.Parameters
                 try
                 {
 
-                    //Содержанеия по колонне 1.Т01 считаются по паре ACN-PO, формулы выданы Алексеем Зинченко 12.06.2020. Воду принимаем равную 0!
-                    //if (TagName == "S11_T01_QC06_CONT" || TagName == "S11_T01_QC05_CONT")
-                    //    _contentValue = mix.GetContent((float)(Temperature.Val_R + DeltaT[0] * 0.1), (float)(Pressure?.Val_R + DeltaP[0] * 0.01 + AtmoPressure * 0.0001f), 3); //Содержание воды в смеси 0%
+                    //Содержание ACN-Water по колонне 1.T04 (доазеотропная концентрация. Configuration code = 1;
+                    if (TagName == "S11_T04_QC01_CONT" || TagName == "S11_T04_QC02_CONT" || TagName == "S11_T04_QC03_CONT" || TagName == "S11_T04_QC04_CONT" || TagName == "S11_T04_QC05_CONT")
+                        _contentValue = mix.GetContent((float)(Temperature.Val_R + DeltaT[0] * 0.1), (float)(Pressure?.Val_R + DeltaP[0] * 0.01 + AtmoPressure * 0.0001f), 1); //Содержание воды в смеси 0%
 
-                    //else if (TagName == "S11_T01_QC04_CONT" || TagName == "S11_T01_QC03_CONT")
-                    //    _contentValue = mix.GetContent((float)(Temperature.Val_R + DeltaT[0] * 0.1), (float)(Pressure?.Val_R + DeltaP[0] * 0.01 + AtmoPressure * 0.0001f), 2); //Содержание воды в смеси 16%
+                    else if (TagName == "S11_T05_QC01_CONT" || TagName == "S11_T05_QC02_CONT" || TagName == "S11_T05_QC03_CONT" || TagName == "S11_T05_QC04_CONT")
+                        _contentValue = mix.GetContent((float)(Temperature.Val_R + DeltaT[0] * 0.1), (float)(Pressure?.Val_R + DeltaP[0] * 0.01 + AtmoPressure * 0.0001f), 2); //Содержание воды в смеси 16%
 
-                    //else if (TagName == "S11_T01_QC02_CONT" || TagName == "S11_T01_QC01_CONT")
-                    //    _contentValue = mix.GetContent((float)(Temperature.Val_R + DeltaT[0] * 0.1), (float)(Pressure?.Val_R + DeltaP[0] * 0.01 + AtmoPressure * 0.0001f), 1); ////Содержание воды в смеси 18%
-
-                    //else
+                    else
                         _contentValue = mix.GetContent((float)(Temperature.Val_R + DeltaT[0] * 0.1), (float)(Pressure?.Val_R + DeltaP[0] * 0.01 + AtmoPressure * 0.0001f), 0); //Pressure + deltaP 1bar (abs/relative)
                 }
                 catch (Exception)
