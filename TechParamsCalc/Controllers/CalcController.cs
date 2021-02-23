0095422c8f2a08360cdc9761a0589c92997f3fb8
artem.khrolenko <artem.khrolenko@gmail.com>
2021-02-23 15:22:22 +0200

    Edit_T06

--- TechParamsCalc/Controllers/CalcController.cs
+++ TechParamsCalc/Controllers/CalcController.cs
@@ -457,7 +457,7 @@ namespace TechParamsCalc.Controllers
                     contentCreator.UpdateItemListFromOpc();
                     foreach (var item in (contentCreator as ContentCreator).ContentList)
                     {
-                        if (!item.IsInValid)
+                        if (!item.IsInValid && item.TagName.Contains("S11_T06_QC02"))
                         {
                             item.CalculateContent();
 
